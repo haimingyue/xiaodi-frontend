d103ccdc7b6fcda0fc4a5aaa2f5224c3b3d3f22e
simoon <420526391@qq.com>
2023-02-06 07:31:28 +0800

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    header
+    <slot />
+    footer
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
