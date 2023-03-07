c5eda3e93413db786136974f5b7cc73fb233260e
simoon <420526391@qq.com>
2023-03-07 15:04:23 +0800

    添加unocss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,44 @@ import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers';
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    modules: ['@unocss/nuxt'],
+    unocss: {
+        uno: true,
+        attributify: true,
+        shortcuts: [
+            { flexc: 'flex items-center justify-center' },
+            { flexb: 'flex items-center justify-between' },
+            { btn: 'rounded-5px text-center cursor-pointer select-none' },
+            { 'text-btn': 'text-center cursor-pointer select-none' },
+        ],
+        rules: [
+            [
+                /^truncate-(\d+)$/,
+                ([, d]) => ({
+                    overflow: 'hidden',
+                    display: '-webkit-box',
+                    'text-overflow': 'ellipsis',
+                    '-webkit-line-clamp': d,
+                    '-webkit-box-orient': 'vertical',
+                })
+            ],
+            [
+                /^center-text-(\d+)$/,
+                ([, d]) => ({
+                    height: `${d}px`,
+                    'align-items': 'center',
+                    'line-height': `${Number(d) -2}px`,
+                })
+            ],
+            [
+                /^wh-(\d+)$/,
+                ([, d]) => ({
+                    width: `${d}px`,
+                    height: `${d}px`,
+                })
+            ]
+        ]
+    },
     experimental: {
         reactivityTransform: true
     },
--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,12 @@
 <template>
   <div class="home" @click="handleClick">
-    <a-button type="primary">Primary Button</a-button>
-    首页11{{ text }}{{ data.xiaodi.a }}
+      <div wh-300 bg-blue font-600 color-red flexc>小滴课堂</div>
+      <a-button type="primary" bg-red!>按钮</a-button>
   </div>
 </template>
 
 <script setup>
-let text = $ref(3)
-const data = reactive({
-  xiaodi: {
-    a: 1
-  }
-})
 
-const handleClick = () => {
-  text = 4
-}
 </script>
 
 <style scoped>
